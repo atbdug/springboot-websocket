0203706d1d80da136545e4bdbe3774702bd97bda
atbdug <atbdug@gmail.com>
2024-02-18 19:08:15 +0800

    把发送消息的用户，id放路径，给指定的用户发送消息

--- src/main/java/com/gj/sendone/MyOneToOneServer.java
+++ src/main/java/com/gj/sendone/MyOneToOneServer.java
@@ -5,8 +5,10 @@ import com.alibaba.fastjson.JSONObject;
 import com.google.gson.Gson;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import javax.websocket.*;
+import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
 import java.io.IOException;
 import java.util.Map;
@@ -17,7 +19,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 /**
  * @author Gjing
  **/
-@ServerEndpoint("/test-one/{chatId}")
+@ServerEndpoint("/test-one/{userId}")
 @Component
 @Slf4j
 public class MyOneToOneServer {
@@ -28,22 +30,24 @@ public class MyOneToOneServer {
     //静态变量，用来记录当前在线连接数。应该把它设计成线程安全的。
     private static AtomicInteger onlineNum = new AtomicInteger();
     private Session session;
+
+    private String userId;
     private final Gson gson = new Gson();
 
     @OnOpen
-    public void onOpen(Session session) {
+    public void onOpen(@PathParam("userId") String userId, Session session) {
         log.info("有新的客户端上线: {}", session.getId());
         this.session = session;
-        clients.put(session.getId(), session);
+        this.userId = userId;
+        clients.put(userId, session);
         int num = onlineNum.incrementAndGet();
         System.out.println("当前在线人数："+num+"人");
     }
 
     @OnClose
-    public void onClose(Session session) {
-        String sessionId = session.getId();
-        log.info("有客户端离线: {}", sessionId);
-        clients.remove(sessionId);
+    public void onClose(@PathParam("userId") String userId,Session session) {
+        log.info("有客户端离线: {}", userId);
+        clients.remove(userId);
         int num = onlineNum.decrementAndGet();
         System.out.println("当前在线人数："+num+"人");
     }
@@ -58,21 +62,50 @@ public class MyOneToOneServer {
         throwable.printStackTrace();
     }
 
+    /**
+     * 广播模式发送消息
+     * @param message
+     * @param session
+     */
+//    @OnMessage
+//    public void onMessage(String message,Session session) {
+//        log.info("收到客户端发来的消息: {}", message);
+//        Message messageBody = new Message();
+//        JSONObject jsonObject = JSONObject.parseObject(message);
+//        messageBody.setMessage(jsonObject.getString("msg"));
+//        messageBody.setToUserId(jsonObject.getString("toUserId"));
+//        messageBody.setUserId(session.getId());
+//        for (Map.Entry<String, Session> entry: this.clients.entrySet()) {
+//            System.out.println("Key = " + entry.getKey() + ", Value = " + entry.getValue());
+//            if (entry.getKey().equals(messageBody.getToUserId())){
+//                messageBody.setUserId(entry.getKey());
+//                this.sendTo(entry.getValue(),messageBody);
+//            }
+//        }
+//    }
+
+    /**
+     * 点对点发送消息
+     * @param userId
+     * @param message
+     * @param session
+     */
     @OnMessage
-    public void onMessage(String message,Session session) {
+    public void onMessage(@PathParam("userId") String userId,String message,Session session) {
         log.info("收到客户端发来的消息: {}", message);
-        String userId = session.getId();
         Message messageBody = new Message();
         JSONObject jsonObject = JSONObject.parseObject(message);
         messageBody.setMessage(jsonObject.getString("msg"));
         messageBody.setToUserId(jsonObject.getString("toUserId"));
-        messageBody.setUserId(session.getId());
-        for (Map.Entry<String, Session> entry: this.clients.entrySet()) {
-            System.out.println("Key = " + entry.getKey() + ", Value = " + entry.getValue());
-            if (entry.getKey().equals(messageBody.getToUserId())){
-                messageBody.setUserId(entry.getKey());
-                this.sendTo(entry.getValue(),messageBody);
-            }
+        messageBody.setUserId(userId);
+        Session formSession = clients.get(userId);
+        Session toSession = clients.get(jsonObject.getString("toUserId"));
+        if (toSession!=null){
+            this.sendTo(toSession,messageBody);
+        }else {
+            messageBody.setMessage("用户"+messageBody.getToUserId()+"不存在");
+            messageBody.setToUserId(userId);
+            this.sendTo(formSession,messageBody);
         }
     }
 
@@ -82,7 +115,7 @@ public class MyOneToOneServer {
      * @param message 消息对象
      */
     private void sendTo(Session session,Message message) {
-        Session s = clients.get(message.getUserId());
+        Session s = clients.get(message.getToUserId());
         if (s != null) {
             try {
                 if (s.getId().equals(message.getUserId())){
